c9d95aa87ab88abb76a95a22dabf01480f96d1e7
sapodecolete <contato.arielinfo@gmail.com>
2025-03-09 16:07:29 -0300

    Mudança no Modle e nas Views

--- MinhasCompras/Views/ListaProduto.xaml.cs
+++ MinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,11 +1,24 @@
+using MinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
-	public ListaProduto()
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+    public ListaProduto()
 	{
 		InitializeComponent();
-	}
+
+        lst_produtos.ItemsSource = lista;
+    }
+
+    protected async override void OnAppearing()
+    {
+        List<Produto> tmp = await App.Db.GetAll();
+
+        tmp.ForEach(i => lista.Add(i));
+    }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
@@ -18,5 +31,30 @@ public partial class ListaProduto : ContentPage
         {
             DisplayAlert("Ops", ex.Message, "OK");
         }
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string q = e.NewTextValue;
+
+        lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        double soma = lista.Sum(i => i.Total);
+
+        string msg = $"O total é {soma:C}";
+
+        DisplayAlert("Total dos Produtos", msg, "OK");
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
     }
 }
